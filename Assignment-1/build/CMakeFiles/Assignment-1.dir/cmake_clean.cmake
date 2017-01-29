af6555fb0fbe42f22b60f92a6b50c012af76a817
elloh755 <elloh755@github.com>
2017-01-28 18:00:23 -0600

    I hope

---
+++ Assignment-1/build/CMakeFiles/Assignment-1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment-1.dir/mfs.c.o"
+  "Assignment-1.pdb"
+  "Assignment-1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment-1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
